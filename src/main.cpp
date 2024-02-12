357f732d6f1d480ed8709e19aeeb6a03bf593202
jennaluz <jpura827@gmail.com>
2024-02-12 13:05:32 -0800

    Implement softAP intialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "lumen/application.hpp"
+#include "lumen/net/wifi.hpp"
+
+#include <cstring>
 
 extern "C" void app_main()
 {
+    lumen::net::init_wifi_softap();
+
     lumen::Application app{};
 }
