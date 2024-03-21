4c43bd16c9f1628f92b63510148fc924248bb35f
loganfin <finley4.logan@gmail.com>
2024-03-20 16:38:40 -1000

    Add mDNS initialization code

---
+++ src/lumen/net/mdns.cpp
@@ -0,0 +1,28 @@
+#include "lumen/net/mdns.hpp"
+
+#include "mdns.h"
+
+namespace lumen::net {
+namespace {
+
+constexpr auto tag = "net/mdns";
+
+} // namespace
+
+void init_mdns()
+{
+    mdns_init();
+    mdns_hostname_set(CONFIG_MDNS_HOSTNAME);
+
+        mdns_txt_item_t serviceTxtData[] = {
+        {"board", "esp32s3"},
+        {"path", "/"}
+    };
+
+    ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, serviceTxtData,
+                                     sizeof(serviceTxtData) / sizeof(serviceTxtData[0])));
+
+
+}
+
+} // namespace lumen::net
