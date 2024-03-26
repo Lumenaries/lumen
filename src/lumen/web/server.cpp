67f677fca7d9db3a0e48544cce258c801ac4614a
jennaluz <jpura827@gmail.com>
2024-03-25 22:51:24 -0700

    Start integrating frontend files

--- src/lumen/web/server.cpp
+++ src/lumen/web/server.cpp
@@ -4,6 +4,10 @@
 
 #include "esp_http_server.h"
 #include "esp_log.h"
+#include "esp_vfs.h"
+
+#include <fcntl.h>
+#include <string>
 
 namespace lumen::web {
 namespace {
@@ -15,6 +19,15 @@ constexpr auto tag = "web/server";
  * \param server A reference to a valid http server handle.
  */
 void register_endpoints(httpd_handle_t& server);
+
+/**
+ *
+ */
+bool check_file_extension(char const* filepath, char const* ext);
+
+/**
+ */
+esp_err_t set_content_type_from_file(httpd_req_t* req, char const* filepath);
 
 } // namespace
 
@@ -42,15 +55,16 @@ namespace {
  */
 esp_err_t root_handler(httpd_req_t* req)
 {
-    constexpr auto html = "<h1>Hello World!</h1>"
-                          "<div>"
-                          "<a style='margin-right: 5px;' href='/increase_one'>increase_one</a>"
-                          "<a href='/decrease_one'>decrease_one</a>"
-                          "</div>"
-                          "<div>"
-                          "<a href='/increase_two'>increase_two </a>"
-                          "<a style='margin-right: 15px;' href='/decrease_two'>decrease_two</a>"
-                          "</div>";
+    constexpr auto html =
+        "<h1>Hello World!</h1>"
+        "<div>"
+        "<a style='margin-right: 5px;' href='/increase_one'>increase_one</a>"
+        "<a href='/decrease_one'>decrease_one</a>"
+        "</div>"
+        "<div>"
+        "<a href='/increase_two'>increase_two </a>"
+        "<a style='margin-right: 15px;' href='/decrease_two'>decrease_two</a>"
+        "</div>";
     httpd_resp_send(req, html, HTTPD_RESP_USE_STRLEN);
 
     return ESP_OK;
@@ -90,6 +104,69 @@ esp_err_t decrease_two_handler(httpd_req_t* req)
     );
 
     return root_handler(req);
+}
+
+esp_err_t common_get_handler(httpd_req_t* req)
+{
+    uint32_t filepath_max = ESP_VFS_PATH_MAX + 128;
+    char filepath[filepath_max] = "/www";
+
+    if (req->uri[strlen(req->uri) - 1] == '/') {
+        strlcat(filepath, "/index.html", sizeof(filepath));
+    } else {
+        strlcat(filepath, req->uri, sizeof(filepath));
+    }
+
+    int fd = open(filepath, O_RDONLY, 0);
+    if (fd == -1) {
+        ESP_LOGE(tag, "Failed to open file : %s", filepath);
+
+        // Respond with 500 Internal Server Error
+        httpd_resp_send_err(
+            req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file"
+        );
+        return ESP_FAIL;
+    }
+
+    set_content_type_from_file(req, filepath);
+
+    uint32_t scratch_buffer_size = 10240;
+    char chunk[scratch_buffer_size];
+    ssize_t read_bytes;
+
+    do {
+        // Read the file in the scratch buffer in chunks
+        read_bytes = read(fd, chunk, scratch_buffer_size);
+
+        if (read_bytes == -1) {
+            ESP_LOGE(tag, "Failed to read file : %s", filepath);
+        } else if (read_bytes > 0) {
+            // Send the buffer contents as HTTP response chunk
+            if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+                close(fd);
+                ESP_LOGE(tag, "File sending failed!");
+
+                // Abort sending file
+                httpd_resp_sendstr_chunk(req, nullptr);
+
+                // Respond with 500 Internal Server Error
+                httpd_resp_send_err(
+                    req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file"
+                );
+
+                return ESP_FAIL;
+            }
+        }
+    } while (read_bytes > 0);
+
+    // Close file after sending complete
+    close(fd);
+    ESP_LOGI(tag, "File sending complete");
+
+    // Respond with an empty chunk to signal HTTP response completion
+    httpd_resp_send_chunk(req, nullptr, 0);
+
+    return ESP_OK;
 }
 
 void register_endpoints(httpd_handle_t& server)
@@ -133,6 +210,40 @@ void register_endpoints(httpd_handle_t& server)
         .user_ctx = nullptr
     };
     httpd_register_uri_handler(server, &decrease_two_uri);
+
+    httpd_uri_t common_get_uri = {
+        .uri = "/*",
+        .method = HTTP_GET,
+        .handler = common_get_handler,
+        .user_ctx = nullptr
+    };
+    httpd_register_uri_handler(server, &common_get_uri);
+}
+
+bool check_file_extension(char const* filepath, char const* ext)
+{
+    return strcasecmp(&filepath[strlen(filepath) - strlen(ext)], ext) == 0;
+}
+
+esp_err_t set_content_type_from_file(httpd_req_t* req, char const* filepath)
+{
+    const char* type = "text/plain";
+
+    if (check_file_extension(filepath, ".html")) {
+        type = "text/html";
+    } else if (check_file_extension(filepath, ".js")) {
+        type = "application/javascript";
+    } else if (check_file_extension(filepath, ".css")) {
+        type = "text/css";
+    } else if (check_file_extension(filepath, ".png")) {
+        type = "image/png";
+    } else if (check_file_extension(filepath, ".ico")) {
+        type = "image/x-icon";
+    } else if (check_file_extension(filepath, ".svg")) {
+        type = "image/svg+xml";
+    }
+
+    return httpd_resp_set_type(req, type);
 }
 
 } // namespace
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,21 @@
 #include "lumen/net/wifi.hpp"
 
 #include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_spiffs.h"
+#include "esp_vfs.h"
 #include "nvs_flash.h"
+
+namespace {
+
+constexpr auto tag = "main";
+
+/**
+ */
+esp_err_t init_fs();
+
+} // namespace
 
 extern "C" void app_main()
 {
@@ -13,8 +27,58 @@ extern "C" void app_main()
     // Initialize NVS needed by Wi-Fi
     ESP_ERROR_CHECK(nvs_flash_init());
 
+    // Create an abstraction layer on top of TCP/IP
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(init_fs());
+
     lumen::net::init_wifi_softap();
     lumen::net::init_mdns();
 
     xTaskCreate(lumen::app_task, "Application Task", 4096, nullptr, 5, nullptr);
 }
+
+namespace {
+
+esp_err_t init_fs()
+{
+    esp_vfs_spiffs_conf_t config = {
+        .base_path = CONFIG_WEB_MOUNT_POINT,
+        .partition_label = nullptr,
+        .max_files = 5,
+        .format_if_mount_failed = false
+    };
+    ESP_LOGI(tag, "%s", CONFIG_WEB_MOUNT_POINT);
+    esp_err_t ret = esp_vfs_spiffs_register(&config);
+
+    switch (ret) {
+    case ESP_OK:
+        break;
+    case ESP_FAIL:
+        ESP_LOGE(tag, "Failed to mount or format filesystem");
+        break;
+    case ESP_ERR_NOT_FOUND:
+        ESP_LOGE(tag, "Failed to find SPIFFS partition");
+        break;
+    default:
+        ESP_LOGE(tag, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+    }
+
+    if (ret != ESP_OK) {
+        return ESP_FAIL;
+    }
+
+    size_t total = 0;
+    size_t used = 0;
+
+    ret = esp_spiffs_info(nullptr, &total, &used);
+    if (ret == ESP_OK) {
+        ESP_LOGI(tag, "Partition size: total: %d, used: %d", total, used);
+    } else {
+        ESP_LOGE(tag, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    }
+
+    return ESP_OK;
+}
+
+} //namespace
